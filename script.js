48ec88322cc558e110c64590da62786ef17a1138
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-02-22 23:32:26 +0000

    adding changes

--- script.js
+++ script.js
@@ -4,23 +4,28 @@ let apiKey = "ddd9f5250efabb4bed328c6729073eb9";
 
 //event listener button create element
 //How to change variable in global scope
+
 document.getElementById("search-button").onclick = search;
 
 
 function search(event) {
     event.preventDefault();
-
     let city = document.getElementById("search-input").value;
     geoCode(city)
+    createBtn(city)
 
 }
 
 // add local storage to keep buttons on screen
 // add data class to button
 // recall data class from local storage
-function createBtn() {
+// add clear button
+// prevent from typing random
+
+function createBtn(city) {
     let newBtn = document.createElement("button");
     let listEl = document.querySelector("#history");
+    //create ul /li
     listEl.append(newBtn);
     newBtn.type = "button"
     newBtn.class = "btn-primary"
@@ -28,13 +33,13 @@ function createBtn() {
     localStorage.setItem(city, `data-${city}`);
 
 }
-//Console log to check how to get weather output // 
 
 function geoCode(city) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
         .then(response => response.json())
         .then(function (cityOutput) {
             let foundCity = cityOutput[0]
+            console.log(foundCity)
             forecast(foundCity)
             current(foundCity)
         })
@@ -42,22 +47,45 @@ function geoCode(city) {
 }
 
 function forecast(data) {
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data.lat}&lon=${data.lon}&appid=${apiKey}`).then(response => response.json()).then(function (fiveDaysOutput) {
-        let fiveDayDisplayEl = document.querySelector("#forecast")
-        fiveDayDisplayEl.textContent = fiveDaysOutput.list[5].dt_txt;
-        console.log(fiveDaysOutput.list[5].main)
-        console.log(fiveDaysOutput.list[5].dt_txt)
-        console.log(fiveDaysOutput.list[5].weather[0].description)
-        console.log(fiveDaysOutput.list[5].weather[0].icon)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${data.lat}&lon=${data.lon}&appid=${apiKey}&units=metric`).then(response => response.json()).then(function (fiveDaysOutput) {
+        let fiveDayDisplayEl = document.querySelector("#forecast");   
+        ;
+        console.log(fiveDaysOutput)
+        
+        
+        
+        
+        for (let i = 1;  i < 40; i++) {  
+            var date = moment.unix(fiveDaysOutput.list[i].dt).format("DD/MM/YYYY")
+            console.log(i)         
+            fiveDayDisplayEl.innerHTML += `<div id="${fiveDaysOutput.city.name}">
+            <p>City: ${fiveDaysOutput.city.name}</p>
+            <p>Date: ${date}</p>
+            <p>Temperature: ${fiveDaysOutput.list[i].main.temp} °C</p>
+            <p>Humidity: ${fiveDaysOutput.list[i].main.humidity} %</p>
+            </div>`
+                                             
+            i+=7
+        }
+           
     })
 
 }
 
 function current(data){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=${apiKey}`).then(response => response.json()).then(function (currentWeather) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=${apiKey}&units=metric`)
+    .then(response => response.json()).then(function (currentWeather) {
+        let todayDiesplayEl = document.querySelector('#today')
+        let todayDate = moment.unix(currentWeather.dt).format("DD/MM/YYYY")
         console.log(currentWeather)
-
+        todayDiesplayEl.innerHTML = `<div id="today-${currentWeather.name}">
+        <p>City: ${currentWeather.name}</p>
+        <p>Date: ${todayDate}</p>
+        <p><img src='http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png'></p>
+        <p>Temperature: ${currentWeather.main.temp} °C</p>
+        <p>Humidity: ${currentWeather.main.humidity} %</p>
+        <p>Wind speed: ${currentWeather.wind.speed} km/h</p>  
+        </div>`
+             
     })
 }
-
-// 
